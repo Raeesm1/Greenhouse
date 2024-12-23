ebd090bbe7fa2e8d7d46ede7ccb6a2f5186b50eb
Raeesm1 <136614360+Raeesm1@users.noreply.github.com>
2024-12-23 11:18:32 +0000

    Add files via upload
    Baud rating set to 9600 and version number for DHT11 library printed to the serial monitor

---
+++ Automated_System.ino
@@ -0,0 +1,11 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.println(DHT11LIB_VERSION);
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
